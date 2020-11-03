59d429de697d1eb9a9fd1911198aa2323a0139b8
sowmyapunuru143 <73069049+sowmyapunuru143@users.noreply.github.com>
2020-11-03 18:24:54 +0530

    Add files via upload

---
+++ chatbot.js
@@ -0,0 +1,66 @@
+function fun1()
+{
+    var product= document.getElementById("search").value;
+   
+    product.toLowerCase();
+    if(product === 'Arts'){
+        window.open("chat4.html");
+    }
+    else if(product==="Science" ){
+        window.open("chat7.html");
+    }
+    else if(product==="Commerce" ){
+    window.open("chat10.html");
+    }
+
+}
+function fun2()
+{
+    var product= document.getElementById("search1").value;
+   
+    product.toLowerCase();
+    if(product === 'Arts Subjects'){
+        window.open("chat5.html");
+    }
+    else if(product==="Arts Career Options" ){
+        window.open("chat6.html");
+    }
+
+}
+function fun3()
+{
+    var product= document.getElementById("search2").value;
+   
+    product.toLowerCase();
+    if(product === 'Science Subjects'){
+        window.open("chat8.html");
+    }
+    else if(product==="Science Career Options" ){
+        window.open("chat9.html");
+    }
+
+}
+function fun4()
+{
+    var product= document.getElementById("search3").value;
+   
+    product.toLowerCase();
+    if(product === 'Commerce Subjects'){
+        window.open("chat11.html");
+    }
+    else if(product==="Commerce Career Options" ){
+        window.open("chat12.html");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
